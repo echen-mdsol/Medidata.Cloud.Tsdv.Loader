2eac86a6e98bb8e5d2e9f332c724b72f5faeb1e2
Cheng Huang <chuang@mdsol.com>
2015-11-24 17:03:36 -0500

    reveal demo backup

--- Medidata.Cloud.ExcelLoader/CellTypeConverters/CellTypeValueBaseConverter.cs
+++ Medidata.Cloud.ExcelLoader/CellTypeConverters/CellTypeValueBaseConverter.cs
@@ -1,5 +1,6 @@
 using System;
 using DocumentFormat.OpenXml.Spreadsheet;
+using System.Text.RegularExpressions;
 
 namespace Medidata.Cloud.ExcelLoader.CellTypeConverters
 {
@@ -16,15 +17,26 @@ namespace Medidata.Cloud.ExcelLoader.CellTypeConverters
 
         public string GetCellValue(object csharpValue)
         {
-            return csharpValue == null ? null : GetCellValueImpl((T) csharpValue);
+            return csharpValue == null ? null : GetCellValueImpl((T)csharpValue);
         }
 
         public object GetCSharpValue(string cellValue)
         {
             return GetCSharpValueImpl(cellValue);
         }
-
-        protected abstract string GetCellValueImpl(T csharpValue);
+        protected bool IsNumeric(string value){
+            var isNumericRegex = new Regex("^(" +
+                                            /*Hex*/ @"0x[0-9a-f]+" + "|" +
+                                            /*Bin*/ @"0b[01]+" + "|" +
+                                            /*Oct*/ @"0[0-7]*" + "|" +
+                                            /*Dec*/ @"((?!0)|[-+]|(?=0+\.))(\d*\.)?\d+(e\d+)?" +")$");
+           return isNumericRegex.IsMatch(value);
+        }
+        //protected abstract T GetCellValueImpl(T csharpValue);
+        protected virtual string GetCellValueImpl(T csharpValue)
+        {
+            return csharpValue.ToString();
+        }
 
         protected abstract T GetCSharpValueImpl(string cellValue);
     }
